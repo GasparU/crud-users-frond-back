31e0eaf9ba33f55ada5accc7d4943ec5c6237408
Jose Gaspar <sainc2016@gmail.com>
2023-06-03 19:08:47 -0500

    first commit

---
+++ src/hooks/useUserCrud.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import axios from "axios";
+import {url_back} from "../../url"
+const useUserCrud = () => {
+  const [users, setUsers] = useState();
+  const url = `${url_back.URL_USERS}`;
+
+  // GET
+
+  const getAllUsers = () => {
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res);
+        setUsers(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // POST
+  const createNewUser = (data) => {
+    axios
+      .post(url, data)
+      .then((res) => getAllUsers())
+      .catch((err) => console.log(err));
+  };
+
+  // DELETE
+
+  const deleteUserBiId = (id) => {
+    const urlDelete = `${url}/${id}`;
+    axios
+      .delete(urlDelete)
+      .then((res) => {
+        console.log(res);
+        getAllUsers();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // UPDATE
+  const updateUserById = (id, data) => {
+    const urlUpdate = `${url}/${id}`;
+    axios
+      .put(urlUpdate, data)
+      .then((res) => {
+        console.log(res);
+        getAllUsers();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return { users, getAllUsers, createNewUser, deleteUserBiId, updateUserById };
+};
+
+export default useUserCrud;
---
+++ url.js
@@ -0,0 +1,3 @@
+export const url_back = {
+    URL_USERS: 'https://user-backend-gaspar.onrender.com/api/v1/users'
+  }
